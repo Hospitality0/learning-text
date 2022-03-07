cc2d1e901fea9fba703d7102ca055d0a5db52e00
rubbish <635497565@qq.com>
2022-03-07 21:28:28 +0800

    python

---
+++ python_text.py
@@ -0,0 +1,7 @@
+str1 = {'color':'red','points':'1'}
+str2 = {'color':'blue','points':'2'}
+str3 = {'color':'green','points':'3'}
+str4 = {'color':'black','points':'4'}
+sum = [str1,str2,str3,str4]
+for a in sum:
+    print(a)    
