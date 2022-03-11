9509ce3c21b5ac6cb0b356751331387d00578b4e
kewei.song <kewei.song@horizon.ai>
2022-03-11 18:34:19 +0800

    python

---
+++ python/fun.py
@@ -0,0 +1,20 @@
+def extern_1(*p):
+    print(f"1_{p}")
+
+def extern_2(*p):
+    print(f"1_{p}")
+
+def extern_3(*p):
+    print(f"1_{p}")
+
+def extern_4(*p):
+    print(f"1_{p}")
+
+
+
+
+
+
+
+
+
--- python/python_text.py
+++ python/python_text.py
@@ -1,24 +1,48 @@
-def rua(tmp):
-    for a in tmp:
-        print(a)
-
-str = ['']
-
-
-
-
-
-
-
-
+class Dog:
+    def __init__(self,name,age):
+        self.NM = name
+        self.AG = age
+        print('---------父类构造函数-----------')
+    def sit(self):
+        print(f'{self.NM} is sitting')
+    def roll_over(self):
+        print(f'{self.NM} is rolling')
+    def chang_pet_name(self,a):
+        self.NM = a
 
 
 
+class small_Dog(Dog):
+    def __init__(self,name,age):
+        super(small_Dog,self).__init__('大挼',2)
+        self.NM = name
+        self.AG = age
+        print('-----------子类构造函数----------------')
+
+
+mysmalldog = small_Dog('小挼',1)
 
 
 
 
 
+
+
+
+'''
+mydog = Dog('rua',6)
+mydog.chang_pet_name('崽崽')
+print(mydog.NM)
+print(mydog.AG)
+mydog.sit()
+mydog.roll_over()
+
+yourdog = Dog('胖胖',21)
+yourdog.chang_pet_name('乌龟')
+print(yourdog.NM)
+print(yourdog.AG)
+yourdog.sit()
+yourdog.roll_over()'''
 
 
 
