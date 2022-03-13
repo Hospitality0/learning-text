86968bf32228110358717889e6bcab61a142df29
rubbish <635497565@qq.com>
2022-03-13 22:49:29 +0800

    python

--- python/python_text.py
+++ python/python_text.py
@@ -1,26 +1,33 @@
-class Dog:
-    def __init__(self,name,age):
-        self.NM = name
-        self.AG = age
-        print('---------父类构造函数-----------')
-    def sit(self):
-        print(f'{self.NM} is sitting')
-    def roll_over(self):
-        print(f'{self.NM} is rolling')
-    def chang_pet_name(self,a):
-        self.NM = a
+class Car:
+    def __init__(self,a,b,c):
+        self.make = a
+        self.model = b
+        self.year = c
+        self.odometer_reading = 600
+        print('----------父类构造函数-----------')
+    def get_descriptive(self):
+        long_name = f"{self.year}{self.make}{self.model}"
+        return long_name.title()
+    def read_odometer(self):
+        print(self.odometer_reading)
+    def updata_odometer(self,d):
+        if d > self.odometer_reading:
+            self.odometer_reading = d
+        else:
+            print('里程表不能调哦')
+    def increment_odometer(self,e):
+        self.odometer_reading = self.odometer_reading + e
+
+class ElectricCar(Car):
+    def __init__(self,a,b,c):
+        super().__init__(a,b,c)
+        print('----------子类构造函数-----------')
+
+my_BYD = ElectricCar('比亚迪','汉EV',2022)
+print(my_BYD.make,my_BYD.model,my_BYD.year)
 
 
 
-class small_Dog(Dog):
-    def __init__(self,name,age):
-        super(small_Dog,self).__init__('大挼',2)
-        self.NM = name
-        self.AG = age
-        print('-----------子类构造函数----------------')
-
-
-mysmalldog = small_Dog('小挼',1)
 
 
 
@@ -29,20 +36,50 @@ mysmalldog = small_Dog('小挼',1)
 
 
 
-'''
-mydog = Dog('rua',6)
-mydog.chang_pet_name('崽崽')
-print(mydog.NM)
-print(mydog.AG)
-mydog.sit()
-mydog.roll_over()
 
-yourdog = Dog('胖胖',21)
-yourdog.chang_pet_name('乌龟')
-print(yourdog.NM)
-print(yourdog.AG)
-yourdog.sit()
-yourdog.roll_over()'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
